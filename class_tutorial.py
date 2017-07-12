192574c9b24f9e755bf0a00586354402e653ea5b
stingjp <stingjp@stanford.edu>
2017-07-12 01:06:46 -0700

    Updated class tutorial file from CodeAcademy

--- class_tutorial.py
+++ class_tutorial.py
@@ -57,3 +57,50 @@ class Equilateral(Triangle):
         self.angle1 = self.angle
         self.angle2 = self.angle
         self.angle3 = self.angle
+
+
+class Car(object):
+    condition = "new"
+
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+
+    def display_car(self):
+        return "This is a %s %s with %d MPG." % (self.color, self.model, self.mpg)
+
+    def drive_car(self):
+        self.condition = "used"
+
+
+class ElectricCar(Car):
+    def __init__(self, battery_type):
+        self.battery_type = battery_type
+
+    def drive_car(self):
+        self.condition = "like new"
+
+
+my_car1 = Car("DeLorean", "silver", 88)
+my_car = ElectricCar("molten salt")
+my_car.model = "toyota"
+my_car.color = 'brown'
+my_car.mpg = 1000
+print(my_car.condition)
+my_car.drive_car()
+print(my_car.condition)
+
+
+class Point3D(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return ("(%d, %d, %d)" % (self.x, self.y, self.z))
+
+
+my_point = Point3D(1, 2, 3)
+print(my_point)
