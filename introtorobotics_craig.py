b43c676120e38683e800b0f5e9efaa98563f5fbc
stingjp <stingjp@stanford.edu>
2017-07-29 00:50:34 -0700

    test code for studying intro to robotics - Craig

--- introtorobotics_craig.py
+++ introtorobotics_craig.py
@@ -1,6 +1,15 @@
 # introtorobotics_craig.py
 import numpy as np
-
+"""
 R = [[0.8660254, -0.5, 0], [0.5, 0.8660254, 0], [0, 0, 1]]
 print(np.linalg.det(R))
 print(-.5*.866)
+"""
+
+"""
+for i in range(0, np.pi, np.pi / 3):
+	print(np.cos(i+90))
+	print(np.sin)
+	"""
+print(np.cos(2*np.pi/3))
+print(np.sin(np.pi/6))
