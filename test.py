05f8f3e3f06340ad16ce5040cf4cf5bc8f7af193
stingjp <stingjp@stanford.edu>
2017-08-13 21:12:53 -0700

    messed around with the startup error

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import numpy as np
 import time
 try:
-	for i in range(100):
-		print(i)
-		time.sleep(1)
+    for i in range(100):
+        print(i)
+        time.sleep(1)
 except KeyboardInterrupt:
-	pass
+    pass
 print('done, save now')
+
+
+# import socket;
+# socket.socket(socket.AF_INET,socket.SOCK_STREAM).connect(("localhost",52265))
